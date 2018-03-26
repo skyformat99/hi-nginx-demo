1820acc0eaed8cd9f61b80a79fa0a7b84ebd33e9
pangpang <pangpang@hi-nginx.com>
2018-03-26 09:53:34 +0800

    update pymd5 demo to python3 support

--- python/pymd5.py
+++ python/pymd5.py
@@ -1,6 +1,6 @@
-import md5
-test = 'hello,md5!'
-m = md5.new()
+import hashlib
+test = b'hello,md5!'
+m = hashlib.md5()
 m.update(test)
 hi_res.status(200)
 hi_res.content("{k} md5 = {v}".format(k=test,v=m.hexdigest()))
