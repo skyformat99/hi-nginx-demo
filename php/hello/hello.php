6213b0b57307f101703c6325d35eb31122e9abe5
pangpang <pangpang@hi-nginx.com>
2018-03-29 18:43:28 +0800

    update php demo

--- php/hello/hello.php
+++ php/hello/hello.php
@@ -3,9 +3,9 @@
 
 require_once 'hi/servlet.php';
 
-class hello implements \hi\servlet {
+class hello implements servlet {
 
-    public function handler(\hi\request &$req, \hi\response &$res) {
+    public function handler(\hi\request $req, \hi\response $res) {
         $res->content = 'hello,world!';
         $res->status = 200;
     }
